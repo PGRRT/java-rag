e5242660eb06b3fe8f44548c4750f48f0a084ed7
oosiriiss <oosiriiss@gmail.com>
2025-10-22 11:59:41 +0200

    Querying RAG API with message history

--- api/api/routes.py
+++ api/api/routes.py
@@ -3,6 +3,7 @@ from typing_extensions import TypedDict
 from fastapi import APIRouter, UploadFile, File
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from typing import Optional
 
 
 import logging
@@ -13,14 +14,16 @@ logger.setLevel(logging.DEBUG)
 rag_router = APIRouter()
 
 
-def dummy_rag_response(conversation_id: int, message: str) -> tuple[str, List[str]]:
+def dummy_rag_response(
+    conversation_id: int, query: str, history: List[str]
+) -> tuple[str, List[str]]:
     """
     Returns example rag response.
     To be used before rag development is complete.
     """
 
     return (
-        f"Example RAG Response for conversation: {conversation_id} with query: {message}",
+        f"Example RAG Response for conversation: {conversation_id} with query: {query}. conversation history size: : {history}",
         [
             "Context 1",
             "Really long context number 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
@@ -40,7 +43,11 @@ class RagResponse(TypedDict):
 
 
 class QueryParams(BaseModel):
-    message: str
+    query: str
+    """User query"""
+
+    message_history: Optional[List[str]] = None
+    """List of previous messages in given conversation"""
 
 
 @rag_router.post("/query/{conversation_id}")
@@ -49,26 +56,29 @@ async def query(conversation_id: int, params: QueryParams) -> JSONResponse:
     Query the RAG system.
     """
 
-    message = params.message
+    query = params.query
+    history = params.message_history if params.message_history else []
 
-    logger.debug(f"/query/ Received message: {message}")
+    logger.debug(f"/query/ Received message: {query} with history size: {len(history)}")
 
     response: RagResponse = {"success": True, "message": "", "contexts": None}
 
     # TODO :: Error handling when rag will be ready
 
     try:
-        message, contexts = dummy_rag_response(conversation_id, message)
+        rag_response, contexts = dummy_rag_response(conversation_id, query, history)
 
-        response["message"] = message
+        response["message"] = rag_response
         response["contexts"] = contexts
 
         return JSONResponse(content=response)
 
     except Exception as e:
         logging.error(f"Error during generation of RAG response. {e}")
+
         response["message"] = "Internal server error when processing query"
         response["success"] = False
+
         return JSONResponse(content=response, status_code=500)
 
 
@@ -91,6 +101,7 @@ async def upload_documents(
     response: UploadResponse = {"success": True, "message": "Files uploaded"}
 
     if not files:
+        logger.debug("No files provided")
         response["success"] = False
         response["message"] = "No files provided"
 
@@ -101,17 +112,19 @@ async def upload_documents(
             f"Processing file: {file.filename} for conversation {conversation_id}"
         )
 
-    return JSONResponse(content=response)
+    return JSONResponse(content=response, status_code=201)
 
 
 class DeleteResponse(TypedDict):
+    status: bool
     message: str
 
 
 @rag_router.delete("/delete/{converastion_id}")
-async def delet_conversation(converastion_id: int) -> DeleteResponse:
+async def delete_conversation(converastion_id: int) -> DeleteResponse:
     """
     Delete all conversation data.
     """
     logger.debug(f"Deleting conversation {converastion_id} data")
-    return {"message": "Collection deleted"}
+
+    return {"status": True, "message": "Collection deleted"}
